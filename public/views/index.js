f911264212c4b681e80a333cc15533f008499cec
DESKTOP-0E86V1G\luism <luis.moreira@lmtronics.co.uk>
2018-04-25 23:47:55 +0100

    manage to get it going althougth may not be the best way needs checking

--- public/views/index.js
+++ public/views/index.js
@@ -1,4 +1,4 @@
-var valuesToPlot = [0, 2, 6, 1, 9, 5, 6, 6, 7, 10]
+var valuesToPlot = [0,2, 6, 1, 9, 5, 6, 6, 7, 8]
 
 var graph1 = document.getElementById('graph1a');
 var graph1Rect = graph1.getBoundingClientRect(); // get the bounding rectangle
@@ -24,14 +24,28 @@ var point = graph1.createSVGPoint();
 var line1 = document.getElementById('line1');
 var x = 0;
 var y = 0;
+var pointab = line1.getAttribute('points');
 for(var i = 0; i < valuesToPlot.length; i++){
+    var x1 = x + (i * xAxisScalingFactor);
+    var y1 = valuesToPlot[i] * yAxisScalingFactor;
     point.x = x + (i * xAxisScalingFactor);
     point.y = valuesToPlot[i] * yAxisScalingFactor;
-    line1.points.appendItem(point); 
-    console.log(point.x);
-    console.log(point.y);
+    console.log('x:' + point.x);
+    console.log('y:' + point.y);
+    console.log(point);
+    var pointa = ` ${x1},${y1} `;
+    console.log(pointa);
+    pointab += pointa;
+    //line1.points.appendItem(pointa); 
+    
 }
-//line1.style.fill = "none";
-//line1.setAttribute("fill", "none");
+console.log(pointab);
+line1.setAttribute('points', pointab);
 
 
+
+//transform='translate(0 300)';
+
+//<g transform="translate(600 600)">
+      // <polygon points="0,0 200,0 100,100" style="fill:lime;stroke:purple;stroke-width:1" />
+    //</g>
